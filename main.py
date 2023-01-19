f5050dd5b98e0740263d484b67af9c39aa2c8bab
connorcinna <connorcinna@gmail.com>
2023-01-18 20:43:42 -0500

    finalized ship turning and added bullet class

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import data
 import tkinter as tk
 from tkinter import messagebox
 
-player_ship = ship.ship((255, 255, 255), data.center)#maybe move this to data
 
+player_ship = ship.ship((255, 255, 255),data.center)#maybe move this to data
 
 def redrawWindow(surface):
 	surface.fill((0,0,0))
@@ -16,7 +16,6 @@ def redrawWindow(surface):
 	#asteroid.draw(surface)
 	pygame.display.update()
 
-	pass
 
 def spawn_asteroid():
 	pass
@@ -30,13 +29,21 @@ def main():
 	flag = True
 	clock = pygame.time.Clock() 
 	while flag: #main game loop
+		for event in pygame.event.get():  #event loop
+			if event.type == pygame.QUIT: #quit the game
+				pygame.quit()
+				sys.exit()
+			if event.type == data.fire_event:
+				#pygame.time.set_timer()
+				pass
+
 		player_ship.turn(data.win) #captures the event loop
+		#player_ship.fire_gun(data.win)
 		#time.delay pauses the program for a time in milliseconds, unsure if this and clock.tick are both needed
 		pygame.time.delay(50)  
 		#clock.tick called once per frame, time is in milliseconds, the argument will delay the game to force it to run at 10 frames per second
 		clock.tick(10) 
 		redrawWindow(data.win) #update frame
-	pass
 
 
 
--- ship.py
+++ ship.py
@@ -2,10 +2,7 @@ import sys
 import os
 import pygame
 import math
-import data
-
-
-#several hard coded, "magic" values in this class, better practice would be to make them programmatic
+import data 
 class ship(object):
 
 	def __init__(self, color, pos):
@@ -21,29 +18,39 @@ class ship(object):
 		self.original_image = self.image
 		self.rotated_rect = self.image.get_rect()
 		self.rotated_rect.center = data.center
-		pass
 	def turn(self, surface): #this captures the turning of the ship with the mouse
-		for event in pygame.event.get():  #event loop
-			if event.type == pygame.QUIT: #quit the game
-				pygame.quit()
-				sys.exit()
-
-			mouse_x, mouse_y = pygame.mouse.get_pos()
-			distance_x, distance_y = mouse_x - self.head[0], mouse_y - self.head[1]			
-			self.direction = (180 / math.pi) * -math.atan2(distance_y, distance_x)
-			self.image = pygame.transform.rotate(self.original_image, int(self.direction))
-			self.rotated_rect = self.image.get_rect()
-			self.rotated_rect.center = data.center
-			self.cosine = math.cos(math.radians(self.direction)+90)
-			self.sine = math.sin(math.radians(self.direction)+90)
-			self.head = (data.center[0] + self.cosine*self.w / 2, data.center[1] - self.sine*self.h / 2)
-			print(self.head)
-		pass
+		mouse_x, mouse_y = pygame.mouse.get_pos()
+		distance_x, distance_y = mouse_x - self.head[0], mouse_y - self.head[1]			
+		self.direction = (180 / math.pi) * -math.atan2(distance_y, distance_x)
+		self.image = pygame.transform.rotate(self.original_image, int(self.direction))
+		self.rotated_rect = self.image.get_rect()
+		self.rotated_rect.center = data.center
+		self.cosine = math.cos(math.radians(self.direction)+90)
+		self.sine = math.sin(math.radians(self.direction)+90)
+		self.head = (data.center[0] + self.cosine*self.w / 2, data.center[1] - self.sine*self.h / 2)
 	def reset(self, pos):
 		pass
-	def fire_gun(self):
-		pass
+	def fire_gun(self, surface):
+		pygame.time.delay(500) #fire every half second
+		pygame.draw.line(surface, (255, 255, 255), self.head, self.head, width = 1)
+
 	def draw(self, surface): #does this need surface
 		data.win.blit(self.image, self.rotated_rect)
 
-		pass
+
+
+
+class bullet(object):
+	
+	def __init__(self, color, pos, ship):
+		self.color = color
+		self.pos = pos
+		self.x = ship.head[0]
+		self.y = ship.head[1]
+		#xv and yv represent velocity of the bullet, based on the angle of head ranges from 0 to 4
+		#may need to make minimum value higher
+		self.xv = 0 
+		self.yv = 0
+		self.max_xv = 4
+		self.max_yv = 4
+
